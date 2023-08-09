6fc70246e1c07164bb574abd5e22c213064c44c7
Bekiboo <julien.connault@gmail.com>
2023-08-09 20:48:22 +0300

    Add customized theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			'space-cadet': '#24295C',
+			'delft-blue': '#38456e',
+			'persian-red': '#d23324' //very similar to red-600
+			// cyan-50
+			// neutral-50
+		},
 		extend: {}
 	},
-	plugins: [require('daisyui')]
-};
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				myLightTheme: {
+					primary: '#d23324',
+					secondary: '#38456e',
+					accent: '#2DD4BF',
+					neutral: '#3d4451',
+					'base-100': '#24295C'
+				}
+			}
+			// 'dark',
+			// 'cupcake'
+		]
+	}
+}
