af425458d08406d6806e5aced598352e247627ee
Bekiboo <julien.connault@gmail.com>
2025-10-04 11:07:40 +0300

    update css config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,11 +7,7 @@
 	let windowWidth = $state(0)
 </script>
 
-<div class="relative text-slate-200 bg-opacity-20 bg-slate-800">
-	<background-gradient
-		class="fixed w-screen h-screen -z-10 bg-gradient-to-br from-slate-950 via-50% bg-opacity-50 via-slate-900 to-[#330a0a]"
-	></background-gradient>
-	<!-- via-50% via-slate-900 -->
+<div class="relative text-slate-200 bg-gradient-to-br from-slate-950 via-50% bg-opacity-50 via-slate-900 to-[#330a0a]">
 	{#if windowWidth > 1024}
 		<GameWrapper>
 			{@render children?.()}
